e87fcc9b1564087af7b268961e1ec831af9f17e0
Norah Say <norahsay@gmail.com>
2019-05-05 22:55:18 -0700

    Adding resuable components to project

---
+++ src/App.js
@@ -0,0 +1,18 @@
+/* eslint-disable no-unused-vars */
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import { Header } from './components/common';
+
+
+class App extends Component {
+  render() {
+    return (
+      <View>
+        <Header headerText="Authentication" />
+        <Text>An app</Text>
+      </View>
+    );
+  }
+}  
+
+export default App;

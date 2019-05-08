4ae6c8a17e9fe4b48845885b9c19ac2ee44628a1
Norah Say <norahsay@gmail.com>
2019-05-08 09:13:08 -0700

    Connect app to firebase

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 /* eslint-disable no-unused-vars */
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase'; 
 import { Header } from './components/common';
 
 
+
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+    apiKey: 'AIzaSyC0rX-L256lWhM15n3N3Aw0DbV7CRwdTEI',
+    authDomain: 'authtutorial-3bccc.firebaseapp.com',
+    databaseURL: 'https://authtutorial-3bccc.firebaseio.com',
+    projectId: 'authtutorial-3bccc',
+    storageBucket: 'authtutorial-3bccc.appspot.com',
+    messagingSenderId: '844040718344',
+    appId: '1:844040718344:web:b12ebdeb912de0fc'
+    });
+  }
+  
   render() {
     return (
       <View>
